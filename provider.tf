3dfaa11dde874234f117c9dda1809b990a3517df
tdufau000 <85114144+tdufau000@users.noreply.github.com>
2021-07-05 12:54:35 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,2 +1,19 @@
-[default]
- region=eu-west-3
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">= 3.35.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "eu-west-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  
+  assume_role {
+    # The role ARN within Account B to AssumeRole into.
+    role_arn = "arn:aws:iam::161331958293:role/OrganizationAccountAccessRole"
+  }
+}
